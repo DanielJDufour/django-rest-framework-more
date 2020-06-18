40f84c699dd6cb81ca189c895000376a50954e07
DanielJDufour <daniel.j.dufour@gmail.com>
2020-06-18 01:03:20 -0400

    added create_model_serializer_class

--- README.md
+++ README.md
@@ -19,5 +19,14 @@ REST_FRAMEWORK = {
 ```
 If you'd like to learn more about using renderers in Django Rest Framework, see https://www.django-rest-framework.org/api-guide/renderers/#setting-the-renderers
 
+#### Creater Model Serializer Class
+```python
+# in serializers.py
+from app.models import Car
+from rest_framework_more.serializers import create_model_serializer_class
+
+CarSerializer = create_model_serializer_class(model=Car)
+```
+
 # contact
 If you have any issues, feel free to post an issue at https://github.com/DanielJDufour/django-rest-framework-more/issues or email the package author at daniel.j.dufour@gmail.com
---
+++ rest_framework_more/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework.serializers import ModelSerializer
+
+def create_model_serializer_class(model, name=None, depth=10, fields='__all__', debug=False):
+    model_name = model.__name__
+    if debug: print("model_name: " + model_name)
+
+    serializer_name = name or model_name + "Serializer"
+    if debug: print("serializer_name:", serializer_name)
+
+    defs = {
+        'Meta': type("Meta", (), { "depth": depth, "model": model, "fields": fields}),
+        '__module__': __name__
+    }
+
+    result = type(serializer_name, (ModelSerializer, ), defs)
+    if debug: print("result:", result)
+
+    return result
